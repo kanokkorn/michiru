f2a128e0e8ea8d18bd3a69d4e10445495db56c69
kanokkorn <kanokorn@outlook.jp>
2020-12-21 12:51:28 +0700

    accept arguments now, but only gray works

--- main.c
+++ main.c
@@ -1,59 +1,44 @@
-/* standard libraries */
-#include <stdio.h>
-#include <unistd.h>
-#include <time.h>
+#include "michiru.h"
 
-/* stb_image */
-#define STB_IMAGE_IMPLEMENTATION
-#include "lib/stb_image.h"
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "lib/stb_image_write.h"
-
-/* function declaration */
-void time_sleep(unsigned int);
-void get_image_data(const char*);
-void image_gray(const char*);
-
-/* 
- * component declaration 
- */
-void conv2d(void);
-
-/* main function */
 int main(int argc, char *argv[]) {
-  printf("<< Michiru - images process with pure C >>\n");
-  if (argv[1] == NULL) {
-    printf("no image input\n");
+  printf("\n<<-- Michiru - Images processing with pure C -->>\n");
+  if (argc == 1 && argv[1] == NULL) {
+    printf("\nUsage: ./michiru [filename] [function]\n"
+           "Function: gray, blur, edge, etc...\n");
+    exit(EXIT_SUCCESS);
+  } else if (argc > 1 && argv[1] == NULL) {
+    printf("no function input\n");
     exit(EXIT_FAILURE);
+  } else if (argc > 2 && argv[1] != NULL && argv[2] != NULL) {
+    if (strcmp("gray", argv[1]) == 0) {
+      printf("\nconverting image to grayscale\n"); 
+      image_gray(argv[2]);
+      printf("done...\n");
+      exit(EXIT_SUCCESS);
+    } else if (strcmp("blur", argv[1]) == 0) { 
+      printf("\napplying gaussian filter to image\n"); 
+      printf("done...\n");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    printf("\nplease specify image input!\n");
   }
-  printf("converting image to grayscale\n"); 
-  image_gray(argv[1]);
-  printf("done...\n");
   return 0;
 }
 
-/* sleep in second */
-void time_sleep(unsigned int sleepVal) {
-  int millisec = 1000 * sleepVal;
-  clock_t start_time = clock();
-  while (clock() < start_time + millisec);
-}
-
-/* get image data */
 void get_image_data(const char* image_file) {
   int width, height, channel;
   unsigned char *img = stbi_load(image_file, &width, &height, &channel, 0);
   if (img == NULL) {
-    printf("Can't open image\n");
+    printf("\nCan't open image\n");
     exit(EXIT_FAILURE);
   }
   printf(
-      "Image loaded!\n%s has %d px width, %d px height and %d channel\n",
+      "\nImage loaded!\n%s has %d px width, %d px height and %d channel\n",
        image_file, width, height, channel
       );
 }
 
-/* blur image */
 void image_blur(const char* image_file) {
   const char *fname = "image_blur.jpg";
   int x, y, comps, blur_chan;
@@ -100,16 +85,4 @@ void image_gray(const char* image_file) {
   }
   stbi_write_jpg(fname, x, y, gray_chan, gray_img, 100);
   stbi_image_free(img); stbi_image_free(gray_img);
-}
-
-/*
- *  component 
- */
-
-void conv2d(void) {
-  const float *kernel[3][3] = {
-    {0, 0, 0},
-    {0, 0, 0},
-    {0, 0, 0}
-  };
 }
