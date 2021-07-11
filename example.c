cc871e786d3516aa19b2161cd1d0396be1e6e4e2
kanokkorn <kanokorn@outlook.jp>
2021-07-11 22:49:02 +0700

    test gpg commit

--- example.c
+++ example.c
@@ -1,6 +1,8 @@
+/* example */
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
+
 #include "michiru.h"
 
 int main (int argc, char ** argv) {
@@ -8,11 +10,12 @@ int main (int argc, char ** argv) {
   while ((opt = getopt(argc, argv, "i:o:")) != 1) {
     switch (opt) {
       case 'i':
-        imgdec(argv[1]);
+        image_info(argv[1]);
         break;
       case 'o':
-        imgdec(argv[1]);
+        image_info(argv[1]);
         break;
-  }
+    }
   return EXIT_SUCCESS;
+  }
 }
